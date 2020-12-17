d5cac29cab2cb069bcccd32c8110c6e054ad0f79
amina2005 <76158751+amina2005@users.noreply.github.com>
2020-12-17 12:20:29 +0100

    Update index.js

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,9 +1,10 @@
-/**
- * sum
- * @param {number} m
- * @param {number} n
- * @returns {number}
- */
+
 function sum(m, n) {
-  // ваш код...
+return m+n;
 }
+console.log (sum (1,1));
+
+function sum(m, n) {
+return m+n;
+}
+console.log (sum (2,5));
