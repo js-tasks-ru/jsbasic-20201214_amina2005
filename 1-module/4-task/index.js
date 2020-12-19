4c8d7386c234ea1dba677740830eae86a93463b4
amina2005 <76158751+amina2005@users.noreply.github.com>
2020-12-19 18:30:37 +0100

    Update index.js

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,8 +1,8 @@
-/**
- * checkSpam
- * @param {string} str base
- * @returns {boolean}
- */
-function checkSpam(str) {
-  // ваш код...
+ function checkSpam(str) {
+    let strToLower = str.toLowerCase();
+    if (strToLower.includes('now') || strToLower.includes('xxx'))
+   {
+   return true;
+        }
+    return false;
 }
