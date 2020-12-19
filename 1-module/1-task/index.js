b44aa33b0feeea7dd1d527ef252042e002ac9789
amina2005 <76158751+amina2005@users.noreply.github.com>
2020-12-19 18:45:50 +0100

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,8 +1,59 @@
-/**
- * Factorial
- * @param {number} n
- * @returns {number}
- */
-function factorial(n) {
-  // ваш код...
+function factorial(n){
+  let answer = 1;
+  if (n == 0 || n == 1){
+    return answer;
+  }else{
+    for(var i = n; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  }  
 }
+let n = 1;
+answer = factorial(n)
+
+
+function factorial(n){
+  let answer = 1;
+  if (n == 0 || n == 1){
+    return answer;
+  }else{
+    for(var i = n; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  }  
+}
+let n = 0;
+answer = factorial(n)
+
+
+function factorial(n){
+  let answer = 1;
+  if (n == 0 || n == 1){
+    return answer;
+  }else{
+    for(var i = n; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  }  
+}
+let n = 5;
+answer = factorial(n)
+
+
+
+function factorial(n){
+  let answer = 1;
+  if (n == 0 || n == 1){
+    return answer;
+  }else{
+    for(var i = n; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  }  
+}
+let n = 3;
+answer = factorial(n)
