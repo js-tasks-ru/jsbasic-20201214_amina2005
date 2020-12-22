8cb5bf0ff5c803adcb342aa5c3be60e934fac5c2
amina2005 <76158751+amina2005@users.noreply.github.com>
2020-12-22 16:00:13 +0100

    Update index.js

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,22 @@
-let calculator = {
-  // ваш код
-};
+   function Calculator() {
+
+  this.read = function() {
+    this.a = +prompt('a?', 3);
+    this.b = +prompt('b?', 5);
+  };
+
+  this.sum = function() {
+    return this.a + this.b;
+  };
+
+  this.mul = function() {
+    return this.a * this.b;
+  };
+}
+
+let calculator = new Calculator();
+calculator.read();
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
