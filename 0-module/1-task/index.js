bf90d874d5c7b0494cc860ff90f472bf5ff5b5bf
Amina2005 <amina2005@hotmail.es>
2021-01-09 08:31:35 +0100

    1st task

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,9 +1,6 @@
-/**
- * sum
- * @param {number} m
- * @param {number} n
- * @returns {number}
- */
 function sum(m, n) {
-  // ваш код...
+let result = m + n;
+return result;
 }
+sum (1,1);
+sum (2,5);
